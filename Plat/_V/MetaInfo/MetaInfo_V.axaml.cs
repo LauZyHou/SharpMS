5fdd80555bd6eb845c083234cd6981e1a9ead697
LauZyHou <java233@foxmail.com>
2021-01-01 16:24:47 +0800

    [feat] meta info test page

---
+++ Plat/_V/MetaInfo/MetaInfo_V.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Plat._V
+{
+    public partial class MetaInfo_V : UserControl
+    {
+        public MetaInfo_V()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
--- Plat/_VM/MainWindow_VM.cs
+++ Plat/_VM/MainWindow_VM.cs
@@ -6,6 +6,10 @@ namespace Plat._VM
 {
     public class MainWindow_VM : ViewModelBase
     {
+        private readonly MetaInfo_VM metaInfo_VM = new MetaInfo_VM();
+
         public string Greeting => "Welcome to SharpMS!";
+
+        public MetaInfo_VM MetaInfo_VM => metaInfo_VM;
     }
 }

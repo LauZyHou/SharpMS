34180cb3307e98d70d7f66975fb650988c23d666
LauZyHou <java233@foxmail.com>
2021-04-10 10:01:39 +0800

    feat: add ProVerif process instantiation model

---
+++ Plat/_T/ProVerif/PvInstantiation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat._T
+{
+    /// <summary>
+    /// ProVerif process instantiation.
+    /// </summary>
+    public class PvInstantiation
+    {
+        private readonly List<PvStatement> statements;
+
+        public PvInstantiation(List<PvStatement> statements)
+        {
+            this.statements = statements;
+        }
+
+        public List<PvStatement> Statements => statements;
+
+        public override string ToString()
+        {
+            string res = "process\n";
+            foreach (PvStatement statement in statements)
+            {
+                res += $"\t{statement}\n";
+            }
+            return res;
+        }
+    }
+}

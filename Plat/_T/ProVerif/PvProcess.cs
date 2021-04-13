f15239bb0e499b7d869ff5379b05cf7909f5a00d
LauZyHou <java233@foxmail.com>
2021-04-13 12:02:42 +0800

    refactor: split pv active statement

--- Plat/_T/ProVerif/PvProcess.cs
+++ Plat/_T/ProVerif/PvProcess.cs
@@ -8,28 +8,66 @@ namespace Plat._T
 {
     /// <summary>
     /// ProVerif process model in memory.
-    /// todo: add more attribute
     /// </summary>
     public class PvProcess
     {
         private readonly string name;
         private readonly List<PvParam> parameters;
-        private readonly PvDeclaration localDeclaration;
+        private readonly List<PvActiveStmt> statements;
 
-        public PvProcess(string name, List<PvParam> parameters, PvDeclaration localDeclaration)
+        /// <summary>
+        /// Construct a new Pv process by name.
+        /// </summary>
+        /// <param name="name">process name</param>
+        public PvProcess(string name)
         {
             this.name = name;
-            this.parameters = parameters;
-            this.localDeclaration = localDeclaration;
+            this.parameters = new List<PvParam>();
+            this.statements = new List<PvActiveStmt>();
         }
 
-        public string Name => name;
-        public PvDeclaration LocalDeclaration => localDeclaration;
-        public List<PvParam> Parameters => parameters;
+        /// <summary>
+        /// Append a parameter to caller Pv process.
+        /// </summary>
+        /// <param name="param">process param</param>
+        /// <returns></returns>
+        public PvProcess WithParam(PvParam param)
+        {
+            this.parameters.Add(param);
+            return this;
+        }
 
+        /// <summary>
+        /// Append a statement to caller Pv process.
+        /// </summary>
+        /// <param name="stmt"></param>
+        /// <returns></returns>
+        public PvProcess WithStmt(PvActiveStmt stmt)
+        {
+            this.statements.Add(stmt);
+            return this;
+        }
+
+        /// <summary>
+        /// Pv process name.
+        /// </summary>
+        public string Name => name;
+        /// <summary>
+        /// Pv process parameters list.
+        /// </summary>
+        public List<PvParam> Parameters => parameters;
+        /// <summary>
+        /// Pv process statements list.
+        /// </summary>
+        public List<PvActiveStmt> Statements => statements;
+
+        /// <summary>
+        /// Observe the caller Pv process in the string perspective.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
-            return $"let {name}({string.Join(", ", parameters)}) = \n{localDeclaration}";
+            return $"let {name}({string.Join(", ", parameters)}) = \n{string.Join("\n\t", statements)}";
         }
     }
 }

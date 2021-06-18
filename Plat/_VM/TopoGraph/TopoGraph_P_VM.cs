d526ca6a437b0da18d9d6bc9affd466222a7ccee
LauZyHou <java233@foxmail.com>
2021-06-18 14:27:12 +0800

    feat: create and delete topo edge

--- Plat/_VM/TopoGraph/TopoGraph_P_VM.cs
+++ Plat/_VM/TopoGraph/TopoGraph_P_VM.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plat._C;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,13 +14,19 @@ namespace Plat._VM
     {
 
         /// <summary>
-        /// 创建拓扑连线
+        /// 创建Liner（拓扑边）
         /// </summary>
         /// <param name="src"></param>
         /// <param name="dst"></param>
         public override void CreateLinker(Anchor_VM src, Anchor_VM dst)
         {
-            throw new NotImplementedException();
+            // 无箭头边
+            Linker_VM linker_VM = new Linker_VM(src, dst, this);
+            src.LinkerVM = dst.LinkerVM = linker_VM;
+            this.DragDrop_VMs.Add(linker_VM);
+            // todo
+
+            ResourceManager.UpdateTip($"Create topology edge on topology graph, from [{src.HostVM}] to [{dst.HostVM}].");
         }
 
         /// <summary>
@@ -28,6 +35,21 @@ namespace Plat._VM
         /// <param name="item"></param>
         public override void DeleteDragDropItem(DragDrop_VM item)
         {
+            // 因为有可能删除active anchor所在的item，特判一下
+            if (this.ActiveAnchorVM != null && this.ActiveAnchorVM.HostVM == item)
+            {
+                this.ActiveAnchorVM = null;
+            }
+            // 视item类别做不同处理
+            if (item is Linker_VM)
+            {
+                Linker_VM linker_VM = (Linker_VM)item;
+                linker_VM.Source.LinkerVM = linker_VM.Dest.LinkerVM = null;
+                this.DragDrop_VMs.Remove(linker_VM);
+                // todo extmsg
+                ResourceManager.UpdateTip($"Remove a topology edge on topology graph panel.");
+                return;
+            }
             throw new NotImplementedException();
         }
     }

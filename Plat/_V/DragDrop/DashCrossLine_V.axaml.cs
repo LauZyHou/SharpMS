4a856a691213a26feb4ef92b9856205789ac1cf1
LauZyHou <java233@foxmail.com>
2021-06-22 21:04:25 +0800

    feat: dash cross line view and view model

---
+++ Plat/_V/DragDrop/DashCrossLine_V.axaml.cs
@@ -0,0 +1,19 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Plat._V
+{
+    public partial class DashCrossLine_V : DragDrop_V
+    {
+        public DashCrossLine_V()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ Plat/_VM/DragDrop/DashCrossLine_VM.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat._VM
+{
+    /// <summary>
+    /// 一边Dash一边Cross的连接线VM
+    /// </summary>
+    public class DashCrossLine_VM : Linker_VM
+    {
+        public DashCrossLine_VM(Anchor_VM source, Anchor_VM dest, DragDrop_P_VM panelVM)
+            :base(source, dest, panelVM)
+        {
+        }
+    }
+}

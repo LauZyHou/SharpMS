c250f59163ecbc80c1f00b853ee26cfdf79ddadc
LauZyHou <java233@foxmail.com>
2021-09-22 16:12:07 +0800

    fix: IK panel grid

---
+++ Plat/_M/IK/AttrPair.cs
@@ -0,0 +1,19 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Plat._M
+{
+    /// <summary>
+    /// 一个Proc/Env的Attr以及另一个Proc/Env的Attr
+    /// </summary>
+    public class AttrPair : ReactiveObject
+    {
+        public Proc ProcA { get; set; }
+        public VisAttr AttrA { get; set; }
+        // todo
+    }
+}
